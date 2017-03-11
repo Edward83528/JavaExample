f10cba4851f47394526ed0f7ebc0556663b75387
u0151051 <u0151051@pig>
2017-03-11 20:33:25 +0800

    2017/3/9

---
+++ JavaExample/src/example5_fibonacci/Fibonacci.java
@@ -0,0 +1,25 @@
+package example5_fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		Scanner a = new Scanner(System.in);
+		System.out.println("你要求幾個費氏數列:");
+		int number = a.nextInt();
+		for (int i = 0; i < number; i++) {
+			System.out.printf("%d \t", fib(i));
+		}
+	}
+
+	static int fib(int n) {
+		if (n <= 1) {
+			return n;
+		} else {
+			return fib(n - 1) + fib(n - 2);
+		}
+
+	}
+
+}

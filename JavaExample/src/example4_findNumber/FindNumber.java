f8b4dabbcc08cc7cd494487c66995cc8ddee74e0
u0151051 <u0151051@pig>
2017-03-11 20:31:44 +0800

    2017/3/8

---
+++ JavaExample/src/example4_findNumber/FindNumber.java
@@ -0,0 +1,28 @@
+package example4_findNumber;
+
+import java.util.Scanner;
+
+public class FindNumber {
+	public static void main(String[] args) {
+		int number[] = { 1, 10, 31, 33, 37, 48, 60, 70, 80 };
+
+		int input = findf();
+
+		for (int i = 0; i < number.length; i++) {
+			if (number[i] == input) {
+				System.out.println(number[i]);
+				break;
+			} else {
+				System.out.println("-1");
+			}
+
+		}
+	}
+
+	static int findf() {
+		Scanner a = new Scanner(System.in);
+		System.out.println("¿é¤J¤@¼Æ:");
+		int find = a.nextInt();
+		return find;
+	}
+}

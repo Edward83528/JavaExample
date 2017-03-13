5508d934c95ea91db3c92c0291839e2ce37a3da3
u0151051 <u0151051@pig>
2017-03-13 22:42:55 +0800

    2017/3/12

--- JavaExample/src/example18_primenumber/Primenumber.java
+++ JavaExample/src/example18_primenumber/Primenumber.java
@@ -3,31 +3,35 @@ package example18_primenumber;
 public class Primenumber {
 
 	public static void main(String[] args) {
-		boolean p = true;
+		print();
+	}
 
-		for (int i = 11; i <= 100; i++) {
-
-			for (int j = 2; j < 100; j++) {
-				if (i % j == 0)
-					p = false;
-				break;
-			}
-
+	static void print() {
+		for (int i = 1; i <= 100; i++) {
+			// 如果10的倍數就輸出換行
 			if (i % 10 == 0) {
-
 				System.out.println(i);
-
 			} else {
-
-				if (p) {
-					System.out.printf("$%d\t", i);
+				if (isprime(i) && i != 1) {
+					System.out.printf("*%d\t", i);
 				} else {
 					System.out.printf("%d\t", i);
 				}
-
 			}
 
 		}
 	}
 
+	static boolean isprime(int a) {
+		boolean b = true;
+		// i<a才不會被自己整數
+		for (int i = 2; i < a; i++) {
+			if (a % i == 0) {
+				b = false;
+				break;
+			}
+		}
+
+		return b;
+	}
 }

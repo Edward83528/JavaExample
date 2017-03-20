489fdac904595864ae3503e0ed30f134e0024447
u0151051 <u0151051@pig>
2017-03-20 13:21:25 +0800

    2017/3/20

--- JavaExample/src/example39_homephoneCheck/HomephoneCheck.java
+++ JavaExample/src/example39_homephoneCheck/HomephoneCheck.java
@@ -1,23 +1,24 @@
-package example37_cellphoneCheck;
+package example39_homephoneCheck;
 
 import java.util.Scanner;
 
-public class CellphoneCheck {
+public class HomephoneCheck {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("請輸入你想確認的手機號碼:");
+		System.out.println("請輸入你想確認的家用電話:");
 		String number = input.nextLine();
 		/*
 		 * .(符合任意字元),x*(x出現0次或多次),x+(x出現一次或多次),x?(x出現一次或完全沒有),x{n}x出現n次,x{n,}
 		 * x至少出現n次,x{n,m} x至少出現n次但不超過m次,x出現1次或完全沒有,可加|或閘(或是)
 		 */
-		if (number.matches("^09[0-9]{2}-[0-9]{6}$")) {
+		if (number.matches("^0[2-8]-[0-9]{8}$")) {
 			System.out.println("格式正確");
 		} else {
 			System.out.printf("格式錯誤\t");
-			System.out.println("範例:0938-415021");
+			System.out.println("範例:04-26272127");
 		}
+
 	}
 
 }
